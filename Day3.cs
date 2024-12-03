95c8ff22fb412610c3b87d9553fdf6f0bddf0a29
Alexander Olofsson <me@ananace.dev>
2024-12-03 18:08:25 +0100

    Trim regexes, use @ strings

--- Day3.cs
+++ Day3.cs
@@ -12,12 +12,12 @@ public class Day3 : IAoC
   public void Input(IEnumerable<string> lines)
   {
     foreach (var line in lines)
-      instructions.AddRange(Regex.Matches(line, "(mul)\\((\\d+),(\\d+)\\)|(do)\\(\\)|(don't)\\(\\)").Select(m => m.Value));
+      instructions.AddRange(Regex.Matches(line,  @"mul\(\d+,\d+\)|do\(\)|don't\(\)").Select(m => m.Value));
   }
 
   public void Part1()
   {
-    var sum = instructions.Select(mul => Regex.Match(mul, "(\\d+),(\\d+)").Groups.Values.Skip(1).Select(g => int.Parse(g.Value))).Select(cc => cc.Aggregate(1, (acc, val) => acc * val)).Sum();
+    var sum = instructions.Select(mul => Regex.Match(mul, @"(\d+),(\d+)").Groups.Values.Skip(1).Select(g => int.Parse(g.Value))).Select(cc => cc.Aggregate(1, (acc, val) => acc * val)).Sum();
     Console.WriteLine($"Sum: {sum}");
   }
   public void Part2()
@@ -31,7 +31,7 @@ public class Day3 : IAoC
       else if (inst.StartsWith("do"))
         enabled = true;
       else if (enabled)
-        sum += Regex.Match(inst, "(\\d+),(\\d+)").Groups.Values.Skip(1).Select(g => int.Parse(g.Value)).Aggregate(1, (acc, val) => acc * val);
+        sum += Regex.Match(inst, @"(\d+),(\d+)").Groups.Values.Skip(1).Select(g => int.Parse(g.Value)).Aggregate(1, (acc, val) => acc * val);
     }
     Console.WriteLine($"Sum: {sum}");
   }
