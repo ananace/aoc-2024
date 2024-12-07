9e6e1a00666ceb3e01b1abd4452e50ab8a7a3fb3
Alexander Olofsson <me@ananace.dev>
2024-12-07 07:45:08 +0100

    Trim day 7 solution

--- Day7.cs
+++ Day7.cs
@@ -5,49 +5,49 @@ public class Day7 : IAoC
 {
   public int Day => 7;
 
-  Dictionary<long, int[]> data = new Dictionary<long, int[]>();
+  List<(long, int[])> data = new List<(long, int[])>();
 
   public void Input(IEnumerable<string> lines)
   {
     foreach (var line in lines)
     {
-      var parts = line.Split(':');
+      var parts = line.Split(':', StringSplitOptions.TrimEntries);
 
-      data[long.Parse(parts.First())] = parts.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(p => int.Parse(p.Trim())).ToArray();
+      data.Add((long.Parse(parts.First()), parts.Last().Split(' ').Select(int.Parse).ToArray()));
     }
   }
 
   public void Part1()
   {
-    var correct = data.Where(kv => CalcPart(kv.Key, kv.Value, 0, 0)).Select(kv => kv.Key).Sum();
+    var correct = data.Where(kv => CalcPart(kv.Item1, kv.Item2)).Select(kv => kv.Item1).Sum();
 
     Console.WriteLine($"Correct: {correct}");
   }
   public void Part2()
   {
-    var correct = data.AsParallel().Where(kv => CalcPart2(kv.Key, kv.Value, 0, 0)).Select(kv => kv.Key).Sum();
+    var correct = data.AsParallel().Where(kv => CalcPart2(kv.Item1, kv.Item2)).Select(kv => kv.Item1).Sum();
 
     Console.WriteLine($"Correct: {correct}");
   }
 
-  public bool CalcPart(long res, int[] num, int it, long carried)
+  public bool CalcPart(long res, Span<int> num, long carried = 0)
   {
-    var next = num[it];
-    if (it == num.Length - 1)
+    var next = num[0];
+    if (num.Length == 1)
       return res == carried + next || res == carried * next;
-    return CalcPart(res, num, it + 1, carried + next) || CalcPart(res, num, it + 1, carried * next);
+    return CalcPart(res, num.Slice(1), carried + next) || CalcPart(res, num.Slice(1), carried * next);
   }
 
-  public bool CalcPart2(long res, int[] num, int it, long carried)
+  public bool CalcPart2(long res, Span<int> num, long carried = 0)
   {
-    var next = num[it];
+    var next = num[0];
     // Get the next 10 logarithm for the number, expand the carried by 10^<next 10log>, add the two together
     // For 123 || 45
     // 45 => 10log(1) + 1 => 2
     // 123 * 10^2 => 12300 + 45 => 12345
     long combined = carried * (long)Math.Pow(10, Math.Floor(Math.Log10(next) + 1)) + next;
-    if (it == num.Length - 1)
+    if (num.Length == 1)
       return res == carried + next || res == carried * next || res == combined;
-    return CalcPart2(res, num, it + 1, carried + next) || CalcPart2(res, num, it + 1, carried * next) || CalcPart2(res, num, it + 1, combined);
+    return CalcPart2(res, num.Slice(1), carried + next) || CalcPart2(res, num.Slice(1), carried * next) || CalcPart2(res, num.Slice(1), combined);
   }
 }
