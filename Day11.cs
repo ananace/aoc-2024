77673ad569c7d089a40c29d367fc0b276329cbc6
Alexander "Ace" Olofsson <ace@haxalot.com>
2024-12-11 08:05:53 +0100

    Add real day 11 solution

--- Day11.cs
+++ Day11.cs
@@ -13,49 +13,50 @@ public class Day11 : IAoC
 
   public void Part1()
   {
-    var mut = stones.ToList();
+    var expanded = TryExpand(stones, 25);
 
-    for (int i = 0; i < 25; ++i)
-      Blink(mut);
-
-    Console.WriteLine($"Stones: {mut.Count}");
+    Console.WriteLine($"Stones: {expanded}");
   }
   public void Part2()
   {
-    var mut = stones.ToList();
+    var expanded = TryExpand(stones, 75);
 
-    Console.Write("Calc: ");
-    for (int i = 0; i < 75; ++i)
-    {
-      var before = mut.Count;
-      Console.Write($"{i} ({mut.Count} ");
-      Blink(mut);
-
-      var after = mut.Count;
-      Console.Write($"{after - before}) ");
-    }
-    Console.WriteLine();
-
-    Console.WriteLine($"Stones: {mut.Count}");
+    Console.WriteLine($"Stones: {expanded}");
   }
 
-  public void Blink(List<long> stones)
+  public long TryExpand(IEnumerable<long> stones, int steps)
   {
-    for (int i = 0; i < stones.Count; ++i)
+    if (steps == 0)
+      return stones.Count();
+
+    return stones.Select(s => TryExpand(s, steps)).Sum();
+  }
+  Dictionary<(long, int), long> cache = new Dictionary<(long, int), long>();
+  public long TryExpand(long stone, int steps)
+  {
+    var key = (stone, steps);
+    if (cache.ContainsKey(key))
+      return cache[key];
+
+    var result = TryExpand(Blink(stone), steps - 1);
+    cache[key] = result;
+    return result;
+  }
+
+  public IEnumerable<long> Blink(long stone)
+  {
+    if (stone == 0)
     {
-      var stone = stones[i];
-      var str = stone.ToString();
-      if (stone == 0)
-        stones[i] = 1;
-      else if (str.Length % 2 == 0)
-      {
-        stones.RemoveAt(i);
-        stones.Insert(i, long.Parse(str[..(str.Length/2)]));
-        ++i;
-        stones.Insert(i, long.Parse(str[(str.Length/2)..]));
-      }
-      else
-        stones[i] *= 2024;
+      yield return 1;
+      yield break;
     }
+    var str = stone.ToString();
+    if (str.Length % 2 == 0)
+    {
+      yield return long.Parse(str[..(str.Length / 2)]);
+      yield return long.Parse(str[(str.Length / 2)..]);
+      yield break;
+    }
+    yield return stone * 2024;
   }
 }
