18ca8e972b5b158031e9b50f307cb909ae6c5aa2
Alexander "Ace" Olofsson <ace@haxalot.com>
2024-12-12 17:37:41 +0100

    Finish day 12 solution

--- Day12.cs
+++ Day12.cs
@@ -13,9 +13,9 @@ public class Day12 : IAoC
     size = (lines.First().Length, lines.Count());
   }
 
-  public void Part1()
+  Dictionary<HashSet<(int,int)>,int> areas = new Dictionary<HashSet<(int,int)>,int>();
+  public void PreCalc()
   {
-    long sum = 0;
     HashSet<(int, int)> visited = new HashSet<(int, int)>();
     for (int y = 0; y < size.Y; ++y)
       for (int x = 0; x < size.X; ++x)
@@ -24,24 +24,33 @@ public class Day12 : IAoC
         if (visited.Contains(at))
           continue;
 
-        var size = Flood((x, y), visited);
-        // Console.WriteLine($"Flooding {x},{y} for {size.Area} area, {size.Perim} perimeter");
-        sum += size.Perim * size.Area;
+        var area = Flood((x, y), visited);
+        areas[area.Area] = area.Perim;
       }
+  }
+
+  public void Part1()
+  {
+    int sum = areas.Select(kv => kv.Key.Count * kv.Value).Sum();
 
     Console.WriteLine($"Fencing total: {sum}");
   }
   public void Part2()
   {
+    int sum = areas.Select(kv => kv.Key.Count * countCorners(kv.Key)).Sum();
+
+    Console.WriteLine($"Fencing total: {sum}");
   }
 
   readonly (int dX, int dY)[] links = new[] { (1, 0), (0, 1), (-1, 0), (0, -1) };
-  public (int Area, int Perim) Flood((int X, int Y) from, HashSet<(int X, int Y)> visited)
+  (HashSet<(int,int)> Area, int Perim) Flood((int X, int Y) from, HashSet<(int X, int Y)> visited)
   {
     char at = map[from.Y * size.X + from.X];
+
+    (HashSet<(int,int)> Area, int Perim) ret = (new HashSet<(int,int)>(), 0);
     visited.Add(from);
-    
-    (int Area, int Perim) ret = (1, 0);
+    ret.Area.Add(from);
+
     foreach (var link in links)
     {
       (int X, int Y) newAt = (from.X + link.dX, from.Y + link.dY);
@@ -57,7 +66,7 @@ public class Day12 : IAoC
           continue;
 
         var nextArea = Flood(newAt, visited);
-        ret.Area += nextArea.Area;
+        ret.Area.UnionWith(nextArea.Area);
         ret.Perim += nextArea.Perim;
       }
       else
@@ -65,6 +74,45 @@ public class Day12 : IAoC
         ret.Perim += 1;
       }
     }
+
+    return ret;
+  }
+
+  readonly (int dX, int dY)[] cornerLinks = new[] { (0, 0), (1, 0), (1, 1), (0, 1) };
+  readonly int[] diagonalValues = new[] { (2 << 0) + (2 << 2), (2 << 1) + (2 << 3) };
+  int countCorners(HashSet<(int X, int Y)> points)
+  {
+    int corners = 0;
+    var bounds = findBounds(points);
+    for (int y = bounds.minY; y < bounds.maxY; ++y)
+      for (int x = bounds.minX; x < bounds.maxX; ++x)
+      {
+        var atPoint = cornerLinks.Select(c => points.Contains((x + c.dX, y + c.dY)));
+        var before = corners;
+        if (atPoint.Where(c => c).Count() % 2 == 1)
+          corners++;
+        else if (diagonalValues.Contains(atPoint.Select((c, i) => c ? (2 << i) : 0).Sum()))
+          corners += 2;
+      }
+
+    return corners;
+  }
+
+  (int minX, int minY, int maxX, int maxY) findBounds(HashSet<(int X, int Y)> points)
+  {
+    (int minX, int minY, int maxX, int maxY) ret = (int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
+    foreach (var point in points)
+    {
+      ret.minX = Math.Min(ret.minX, point.X);
+      ret.minY = Math.Min(ret.minY, point.Y);
+      ret.maxX = Math.Max(ret.maxX, point.X);
+      ret.maxY = Math.Max(ret.maxY, point.Y);
+    }
+
+    ret.minX--;
+    ret.minY--;
+    ret.maxX++;
+    ret.maxY++;
 
     return ret;
   }
